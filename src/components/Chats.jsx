b0e786379d4e69d91a2462d99f8c260e4f7cf603
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-23 22:56:57 +0530

    user input in chat section design 1

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,6 +3,8 @@ import informationIcon from '../assets/information.png';
 import videoCameraIcon from '../assets/videoCamera.png';
 import avatarIcon from '../assets/avatarIcon.png';
 import phoneCallIcon from '../assets/phoneCall.png';
+import sendIntelIcon from '../assets/fighterJet.png';
+import emojisIcon from '../assets/fires.png';
 
 function Chats() {
   return (
@@ -38,8 +40,31 @@ function Chats() {
           />
         </div>
       </div>
-      <div className='UserChatDetailsInChatsWindow'></div>
-      <div className='UserInputInChatsWindow'></div>
+      <div className='UserChatDetailsInChatsWindow'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad vitae illo eveniet ratione tempora, pariatur debitis. Enim doloribus eum, nihil dolor nulla veritatis. Nobis, error ipsam sint numquam earum ea iure cum ut quisquam accusantium odit ab unde repudiandae dignissimos officia placeat dolorum odio tempore. Deserunt animi ullam omnis fuga temporibus totam aut atque, delectus itaque accusamus porro ea dolorum. Ratione blanditiis placeat ducimus eligendi fugiat odio molestias ut, dolore natus quibusdam nisi fugit nulla maiores provident necessitatibus facilis ex commodi vero nemo tenetur recusandae. Odit dolorem dolorum, laudantium eius, sint exercitationem veniam, eaque unde fugit placeat voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet tenetur, iure, sapiente deserunt nisi exercitationem incidunt itaque minima eius est autem debitis nihil optio quaerat, id suscipit porro ex qui quos ad odit doloribus? Eius at consequatur voluptates labore iure nihil? Aperiam suscipit quis accusantium sint aliquam dicta deleniti delectus a sed architecto. Sit, mollitia sed eius laboriosam earum culpa quam praesentium harum est neque omnis tenetur dolorem exercitationem magni ipsa veniam modi explicabo odio qui? Deserunt molestias excepturi qui, nulla reiciendis rem accusamus explicabo voluptas vero non provident, fuga odio nihil quis facere consectetur quia molestiae voluptatem vel!
+      </div>
+      <div className='UserInputInChatsWindow flex'>
+        <div className='icons'></div>
+        <input
+          type='text'
+          className='w-[80%] rounded-full pl-4 text-black'
+          placeholder='Your Intel goes here...'
+        />
+        <div title='Emoji' className='emoji'>
+          <img
+            src={emojisIcon}
+            className='w-9 h-9 mx-2 cursor-pointer '
+            alt=''
+          />
+        </div>
+        <div title='Send Intel' className='SendButton'>
+          <img
+            src={sendIntelIcon}
+            className='w-9 h-9 mx-2 cursor-pointer '
+            alt=''
+          />
+        </div>
+      </div>
     </div>
   );
 }
