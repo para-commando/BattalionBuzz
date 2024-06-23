3de8083771f0e315b4ed669fa4fa09c22f68f1a5
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-23 11:19:58 +0530

    search section and chatlist section design

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -6,14 +6,14 @@ import avatarIcon from '../assets/avatarIcon.png';
 
 function UserInfo() {
   return (
-    <div className='flex items-center gap-14 '>
-      <div className='user'>
+    <div className='flex items-center gap-14 mb-7'>
+      <div className='user flex items-center'>
         <img
           src={avatarIcon}
           className='w-14 h-14 mx-2  rounded-full cursor-pointer'
           alt=''
         />
-        <h2>Major Vihaan</h2>
+        <h2 className='font-bold text-xl'>Major Vihaan</h2>
       </div>
       <div className='icons flex'>
         <img src={editIcon} className='w-8 h-8 mx-2 cursor-pointer ' alt='' />
