b2014126277504367dd68e9bdcd2aac2ef84727b
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-23 23:48:20 +0530

    user input section design

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import informationIcon from '../assets/information.png';
 import videoCameraIcon from '../assets/videoCamera.png';
 import avatarIcon from '../assets/avatarIcon.png';
 import phoneCallIcon from '../assets/phoneCall.png';
 import sendIntelIcon from '../assets/fighterJet.png';
 import emojisIcon from '../assets/fires.png';
+import menuIcon from '../assets/menu.png';
+import documentsIcon from '../assets/docs.png';
+import CameraIcon from '../assets/camera.png';
+import mediaIcon from '../assets/media.png';
+import microphoneIcon from '../assets/microphone.png';
 
 function Chats() {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const toggleOptions = () => {
+    setShowOptions(!showOptions);
+  };
   return (
-    <div className='border-r-2 w-[650px] px-5 '>
-      <div className='flex justify-between border-b-2 pb-4'>
+    <div className='border-r-2 w-[670px] px-7 relative flex flex-col items-center'>
+      <div className='flex w-full justify-between border-b-2 pb-4 relative'>
         <div className='UserInfoInChatsWindow flex items-center gap-3'>
           <img
             src={avatarIcon}
@@ -40,28 +50,88 @@ function Chats() {
           />
         </div>
       </div>
-      <div className='UserChatDetailsInChatsWindow'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad vitae illo eveniet ratione tempora, pariatur debitis. Enim doloribus eum, nihil dolor nulla veritatis. Nobis, error ipsam sint numquam earum ea iure cum ut quisquam accusantium odit ab unde repudiandae dignissimos officia placeat dolorum odio tempore. Deserunt animi ullam omnis fuga temporibus totam aut atque, delectus itaque accusamus porro ea dolorum. Ratione blanditiis placeat ducimus eligendi fugiat odio molestias ut, dolore natus quibusdam nisi fugit nulla maiores provident necessitatibus facilis ex commodi vero nemo tenetur recusandae. Odit dolorem dolorum, laudantium eius, sint exercitationem veniam, eaque unde fugit placeat voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet tenetur, iure, sapiente deserunt nisi exercitationem incidunt itaque minima eius est autem debitis nihil optio quaerat, id suscipit porro ex qui quos ad odit doloribus? Eius at consequatur voluptates labore iure nihil? Aperiam suscipit quis accusantium sint aliquam dicta deleniti delectus a sed architecto. Sit, mollitia sed eius laboriosam earum culpa quam praesentium harum est neque omnis tenetur dolorem exercitationem magni ipsa veniam modi explicabo odio qui? Deserunt molestias excepturi qui, nulla reiciendis rem accusamus explicabo voluptas vero non provident, fuga odio nihil quis facere consectetur quia molestiae voluptatem vel!
+      <div className='UserChatDetailsInChatsWindow relative w-full'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad vitae illo
+        eveniet ratione tempora, pariatur debitis. Enim doloribus eum, nihil
+        dolor nulla veritatis. Nobis, error ipsam sint numquam earum ea iure cum
+        ut quisquam accusantium odit ab unde repudiandae dignissimos officia
+        placeat dolorum odio tempore. Deserunt animi ullam omnis fuga temporibus
+        totam aut atque, delectus itaque accusamus porro ea dolorum. Ratione
+        blanditiis placeat ducimus eligendi fugiat odio molestias ut, dolore
+        natus quibusdam nisi fugit nulla maiores provident necessitatibus
+        facilis ex commodi vero nemo tenetur recusandae. Odit dolorem dolorum,
+        laudantium eius, sint exercitationem veniam, eaque unde fugit placeat
+        voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Eveniet tenetur, iure, sapiente deserunt nisi exercitationem incidunt
+        itaque minima eius est autem debitis nihil optio quaerat, id suscipit
+        porro ex qui quos ad odit doloribus? Eius at consequatur voluptates
+        labore iure nihil? Aperiam suscipit quis accusantium sint aliquam dicta
+        deleniti delectus a sed architecto. Sit, mollitia sed eius laboriosam
+        earum culpa quam praesentium harum est neque omnis tenetur dolorem
+        exercitationem magni ipsa veniam modi explicabo odio qui? Deserunt
+        molestias excepturi qui, nulla reiciendis rem accusamus explicabo
+        voluptas vero non provident, fuga odio nihil quis facere consectetur
+        quia molestiae voluptatem vel!
       </div>
-      <div className='UserInputInChatsWindow flex'>
-        <div className='icons'></div>
+      <div className='UserInputInChatsWindow flex items-center bg-green-950 rounded-full px-2 h-14  box-border w-[98%] absolute bottom-2  '>
         <input
           type='text'
-          className='w-[80%] rounded-full pl-4 text-black'
+          className='w-[70%] rounded-full pl-4 text-black h-9 text-lg'
           placeholder='Your Intel goes here...'
         />
-        <div title='Emoji' className='emoji'>
+        <div title='More' className='MoreButton invert'>
           <img
-            src={emojisIcon}
-            className='w-9 h-9 mx-2 cursor-pointer '
-            alt=''
+            src={menuIcon}
+            className='w-9 h-9 mx-2 cursor-pointer'
+            alt='More'
+            onClick={toggleOptions}
           />
         </div>
-        <div title='Send Intel' className='SendButton'>
+        {showOptions && (
+          <div className='options flex items-center'>
+            <div title='emojis' className='emojisOption'>
+              <img
+                src={emojisIcon}
+                className='w-9 h-9 mx-1 cursor-pointer'
+                alt='emojis'
+              />
+            </div>
+            <div title='Documents' className='DocumentOption invert'>
+              <img
+                src={documentsIcon}
+                className='w-7 h-7 mx-1 cursor-pointer'
+                alt='Documents'
+              />
+            </div>
+            <div title='Media' className='Media invert'>
+              <img
+                src={mediaIcon}
+                className='w-7 h-7 mx-2 cursor-pointer'
+                alt='Media'
+              />
+            </div>
+          </div>
+        )}
+        <div title='microphone' className='Microphone invert'>
+          <img
+            src={microphoneIcon}
+            className='w-7 h-7 mx-2 cursor-pointer'
+            alt='microphone'
+          />
+        </div>
+
+        <div title='Camera' className='Camera invert'>
+          <img
+            src={CameraIcon}
+            className='w-7 h-7 mx-2 cursor-pointer'
+            alt='Camera'
+          />
+        </div>
+        <div title='Send' className='SendButton'>
           <img
             src={sendIntelIcon}
-            className='w-9 h-9 mx-2 cursor-pointer '
-            alt=''
+            className='w-9 h-9 mx-2 cursor-pointer'
+            alt='Send'
           />
         </div>
       </div>
