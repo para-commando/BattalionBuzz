584af6f9d4d8443430d772bb1177925a998fb43b
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-23 11:28:15 +0530

    border separation for chatslist

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,7 +3,7 @@ import UserInfo from './UserInfo'
 import ChatList from './ChatList'
 function List() {
   return (
-    <div className=''>
+    <div className='border-r-2 pr-3'>
      <UserInfo />
      <ChatList />
     </div>
