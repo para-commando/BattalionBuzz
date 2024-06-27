a4b04ee09652911951d6ab0cb680840354220d09
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-28 00:24:46 +0530

    login form design 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,19 +13,59 @@ function Login() {
   console.log(watch('example')); // watch input value by passing the name of it
   return (
     <div>
-      <div className='item'>
-        <h2>Welcome back,</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input className='text-black' defaultValue='' placeholder='Enter your call sign...' {...register('Call Sign',{ required: true, min: 3, max: 10, pattern: /^[A-Za-z]+$/i }) } />
+      <div className='item flex flex-col gap-14 justify-center items-center'>
+        <h1 className='text-3xl font-bold p-5'>Login to your account,</h1>
+        <div className='loginFormClass border-2 p-5 '>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='flex flex-col justify-center items-center'
+          >
+            <div className='mb-5 flex-col justify-center items-center'>
+              <input
+                className='text-black w-[548px] p-1 rounded-xl text-center'
+                defaultValue=''
+                placeholder='Enter your call sign...'
+                {...register('callSign', {
+                  required: true,
+                  min: 3,
+                  max: 10,
+                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/,
+                })}
+              />
+              <div className='text-red-500 w[548px] flex justify-center items-center'>
+                {errors.callSign && <span>Invalid Call Sign</span>}
+              </div>
+            </div>
 
-          {/* include validation with required or other standard HTML validation rules */}
-          <input className='text-black' {...register('exampleRequired', { required: true })} />
-          {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
-
-          <input type='submit' />
-        </form>
+            <div className='mb-5 flex flex-col justify-center items-center'>
+              <input
+                className='text-black w-[548px] p-1 rounded-xl text-center'
+                {...register('password', {
+                  required: true,
+                  min: 7,
+                  max: 20,
+                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+                })}
+                placeholder='Password'
+                type='password'
+              />
+              <div className='flex justify-center items-center'>
+                {errors.password && (
+                  <span className='text-red-500 w[548px] text-wrap break-words'>
+                    Password must contain atleast one uppercase, one lowercase,
+                    one number and one special character
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className='flex justify-center'>
+              <input
+                className='bg-green-800 px-9 py-2 rounded-3xl cursor-pointer'
+                type='submit'
+              />
+            </div>
+          </form>
+        </div>
       </div>
       <div className='separator'></div>
       <div className='item'></div>
