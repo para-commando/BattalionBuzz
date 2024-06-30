0a863f14839c75b1b6950fa38f59995abd383cd5
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-30 10:43:05 +0530

    userAuthentication function setup complete with mock data

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 
 import { useForm } from 'react-hook-form';
-
+import { isUserNew, isUserValidated } from '../redux/reducers/userAuth';
+import { useSelector, useDispatch } from 'react-redux';
 function SignUp() {
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    dispatch(isUserValidated(true));
+
+    console.log(data);
+  };
   console.log(watch('example')); // watch input value by passing the name of it
   return (
-
     <div>
       <div className='item flex flex-col gap-14 justify-center items-center'>
         <h1 className='text-3xl font-bold p-5'>Create new account,</h1>
@@ -26,13 +32,13 @@ function SignUp() {
             <div className='mb-5 flex-col justify-center items-center'>
               <input
                 className='text-black w-[548px] p-1 rounded-xl text-center'
-                defaultValue=''
+                defaultValue='GhatakCommandoOne'
                 placeholder='Enter your call sign...'
                 {...register('callSign', {
                   required: true,
                   min: 3,
-                  max: 10,
-                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/,
+                  max: 30,
+                  pattern: /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]+$/,
                 })}
               />
               <div className='text-red-500 w[548px] flex justify-center items-center'>
@@ -42,7 +48,7 @@ function SignUp() {
             <div className='mb-5 flex-col justify-center items-center'>
               <input
                 className='text-black w-[548px] p-1 rounded-xl text-center'
-                defaultValue=''
+                defaultValue='GhatakForce'
                 placeholder='Enter your Regiment...'
                 {...register('regiment', {
                   required: true,
@@ -58,25 +64,25 @@ function SignUp() {
             <div className='mb-5 flex-col justify-center items-center'>
               <input
                 className='text-black w-[548px] p-1 rounded-xl text-center'
-                defaultValue=''
+                defaultValue='8296321779'
                 placeholder='Enter your mobile number...'
-                type="number"
+                type='text'
                 {...register('mobileNumber', {
                   required: true,
-                  min: 10,
-                  max: 10,
-                  valueAsNumber: true,
-                  pattern: /^\\d{10}$/,
+                  pattern: /^\d{10}$/,
                 })}
               />
-              <div className='text-red-500 w[548px] flex justify-center items-center'>
-                {errors.mobileNumber && <span>Invalid mobile number</span>}
+              <div className='text-red-500 w-[548px] flex justify-center items-center'>
+                {errors.mobileNumber && (
+                  <span>{errors.mobileNumber.message}</span>
+                )}
               </div>
             </div>
             <div className='mb-5 flex flex-col justify-center items-center'>
               <div className='relative w-[548px]'>
                 <input
                   className='text-black w-full p-1 rounded-xl text-center'
+                  defaultValue='Password3453@'
                   {...register('password', {
                     required: true,
                     minLength: 7,
@@ -108,9 +114,11 @@ function SignUp() {
               <div className='relative w-[548px]'>
                 <input
                   className='text-black w-full p-1 rounded-xl text-center'
+                  defaultValue='Password3453@'
                   {...register('confirmPassword', {
                     required: true,
-                    validate: (value) => value === watch('password') || 'Passwords do not match',
+                    validate: (value) =>
+                      value === watch('password') || 'Passwords do not match',
                   })}
                   placeholder='Confirm Password'
                   type={showConfirmPassword ? 'text' : 'password'}
@@ -136,7 +144,6 @@ function SignUp() {
                 className='bg-green-800 px-9 py-2 rounded-3xl cursor-pointer'
                 type='submit'
               />
-             
             </div>
           </form>
         </div>
