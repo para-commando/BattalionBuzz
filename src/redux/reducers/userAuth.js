fda67973d84e8c1bfb187dd6d73fc275a53ec744
para-commando <anirudh.nayak.172k@gmail.com>
2024-08-10 19:29:26 +0530

    add users feat optimization, forward message feat integ p1

--- src/redux/reducers/userAuth.js
+++ src/redux/reducers/userAuth.js
@@ -1,105 +1,46 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
-export const loginUser = createAsyncThunk(
-  'userAuth/loginUser',
-  async (data) => {
-    return {
-      data: {
-        name: 'Major Vihaan',
-        profileImg:
-          'https://i.pinimg.com/550x/7d/b1/f1/7db1f1ee7dfcb3795d4ef2db6b8bb3df.jpg',
-      },
-      chats: [
-        {
-          name: 'Major Videep',
-          hasSentMessage: true,
-          profileImg:
-            'https://preview.redd.it/a-commando-from-the-elite-garud-special-forces-of-the-air-v0-ubqtdla4toja1.jpg?width=1080&crop=smart&auto=webp&s=c5dbb3466fef9dd74a111bf9df83e470c8917f43',
-        },
-        {
-          name: 'Major Aarav',
-          hasSentMessage: true,
-          profileImg: 'https://i.redd.it/z2evjlhpobm51.jpg',
-        },
-        {
-          name: 'Major Arjun',
-          hasSentMessage: true,
-          profileImg:
-            'https://img.freepik.com/premium-photo/indian-army-commando_964707-24.jpg',
-        },
-        {
-          name: 'Major Rohan',
-          hasSentMessage: true,
-          profileImg:
-            'https://girlandworld.com/wp-content/uploads/2017/09/576732_407229076040431_2039671248_n.jpg?w=768',
-        },
-        {
-          name: 'Major Sandeep',
-          hasSentMessage: false,
-          profileImg:
-            'https://girlandworld.com/wp-content/uploads/2017/09/nsg-commando-2.jpg?w=768',
-        },
-        {
-          name: 'Major Amit',
-          hasSentMessage: false,
-          profileImg:
-            'https://thelogicalindian.com/h-upload/2020/04/08/170917-armyweb.jpg',
-        },
-        {
-          name: 'Major Karan',
-          hasSentMessage: false,
-          profileImg:
-            'https://akm-img-a-in.tosshub.com/indiatoday/images/photogallery/201305/uniform-1-5_051913114609.jpg?VersionId=NtSbHqYyRNAev91xGpKRX6RIRMExJNdI&size=686:*',
-        },
-        {
-          name: 'Major Manish',
-          hasSentMessage: false,
-          profileImg:
-            'https://e0.pxfuel.com/wallpapers/314/341/desktop-wallpaper-royal-marine-indian-army-special-forces-royal-marine-commando-marine-commandos.jpg',
-        },
-        {
-          name: 'Major Vijay',
-          hasSentMessage: false,
-          profileImg:
-            'https://www.iadb.in/wp-content/uploads/2021/05/Indian-Special-Forces-scaled.jpeg',
-        },
-        {
-          name: 'Major Rajesh',
-          hasSentMessage: false,
-          profileImg:
-            'https://www.mathrubhumi.com/image/contentid/policy:1.4871015:1644554133/image.jpg?$p=0f6e831&&q=0.8',
-        },
-      ],
-    };
+export const getAllUserIds = createAsyncThunk(
+  'userAuth/getAllUserIds',
+  async () => {
+    const querySnapshot = await getDocs(collection(db, 'users'));
+
+    const docIds = querySnapshot.docs.map((doc) => {
+      console.log("🚀 ~ docIds ~ doc:", doc.data())
+
+      return { id: doc.data().id, callSign: doc.data().callSign, img: doc.data().imgUrl };
+    });
+    console.log('🚀 ~ returnnewPromise ~ documentIds:', docIds);
+    return docIds;
   }
 );
 export const fetchUserDetails = createAsyncThunk(
   'userAuth/fetchUserDetails',
   async (data) => {
     return new Promise(async (resolve, reject) => {
-     try {
-       if (!data || !data?.uid) {
-        return resolve({
-           data: { id: '' },
-         });
-       }
-         
-       const docRef = doc(db, 'users', data.uid);
-       const docSnap = await getDoc(docRef);
-       console.log('🚀 ~ returnnewPromise ~ docSnap:', docSnap);
-       if (docSnap.exists()) {
-       return  resolve({
-           data: docSnap.data(),
-         });
-       } else {
-       return  reject({
-           msg: 'no data found',
-         });
-       }
-     } catch (error) {
-      debugger
-     }
+      try {
+        if (!data || !data?.uid) {
+          return resolve({
+            data: { id: '' },
+          });
+        }
+
+        const docRef = doc(db, 'users', data.uid);
+        const docSnap = await getDoc(docRef);
+        console.log('🚀 ~ returnnewPromise ~ docSnap:', docSnap);
+        if (docSnap.exists()) {
+          return resolve({
+            data: docSnap.data(),
+          });
+        } else {
+          return reject({
+            msg: 'no data found',
+          });
+        }
+      } catch (error) {
+        debugger;
+      }
     });
   }
 );
@@ -116,6 +57,7 @@ export const userAuthReducers = createSlice({
     sharedVideos: [],
     sharedAudios: [],
     sharedDocuments: [],
+    allUserIds: [],
   },
   reducers: {
     isUserNew: (state, action) => {
@@ -150,16 +92,16 @@ export const userAuthReducers = createSlice({
           fileName = null,
           audioFileName = null,
           imageName = null,
-          videoName=null
+          videoName = null,
         }) => {
           if (video) {
-            state.sharedVideos.push({video,videoName});
+            state.sharedVideos.push({ video, videoName });
           }
           if (audioURL) {
             state.sharedAudios.push({ audioURL, audioFileName });
           }
           if (image) {
-            state.sharedImages.push({image,imageName});
+            state.sharedImages.push({ image, imageName });
           }
           if (pdf) {
             state.sharedDocuments.push({ pdf, fileName });
@@ -196,6 +138,16 @@ export const userAuthReducers = createSlice({
         state.valueScreenLoading = false;
         alert('Something went wrong, please try again');
         //  state.valueIsSubmitting = false;
+      })
+      .addCase(getAllUserIds.pending, (state) => {
+        console.log('🚀 ~ .addCase ~ state1:', state);
+      })
+      .addCase(getAllUserIds.fulfilled, (state, action) => {
+        debugger;
+        state.allUserIds = action.payload;
+      })
+      .addCase(getAllUserIds.rejected, (state) => {
+        debugger;
       });
   },
 });
